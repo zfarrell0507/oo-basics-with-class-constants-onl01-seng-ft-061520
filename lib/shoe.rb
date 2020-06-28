1adc54b4ae81c37e98af653ddd3a3b16128e17c0
Zachary Farrell <zfarrell0507@gmail.com>
2020-06-28 20:54:19 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,14 +1,46 @@
 class Shoe
-  attr_accessor :color, :size, :material, :condition
-  attr_reader :brand
-
-  def initialize(brand)
-    @brand = brand
+  attr_accessor :color, :size, :material, :condition  # remove the attr_accessor for genre
+  attr_reader :brand, :unique_brand # add an attr_reader for genre
+ 
+  BRANDS = []
+ 
+  def initialize(brands)
+    @brands = brands
   end
-
-  def cobble
-    self.condition = "new"
-    puts "Your shoe is as good as new!"
+  
+  brands.each do |brand|
+    Shoe.new(brand)
   end
-
+ 
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+ 
+  # create the writer for genre and add the logic for the class constant
+  def brands=(brands)
+    @brands = brands
+    BRANDS << brands 
+  end
 end
+
+
+    it 'keeps track of all brands' do
+      brands = ["Uggs", "Rainbow"]
+      brands.each do |brand|
+        Shoe.new(brand)
+      end
+
+      brands.each do |brand|
+        expect(Shoe::BRANDS).to include(brand)
+      end
+    end
+
+    it 'only keeps track of unique brands' do
+      Shoe::BRANDS.clear
+      brands = ["Uggs", "Rainbow", "Nike", "Nike"]
+      brands.each do |brand|
+        Shoe.new(brand)
+      end
+
+      expect(Shoe::BRANDS.size).to eq(3)
+    end
