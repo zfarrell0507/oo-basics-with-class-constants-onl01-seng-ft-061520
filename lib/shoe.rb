c3a78721778bae1e00dd1f7295facc0df0a23fe8
Zachary Farrell <zfarrell0507@gmail.com>
2020-06-28 21:01:17 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -22,25 +22,3 @@ class Shoe
     BRANDS << brands 
   end
 end
-
-
-    it 'keeps track of all brands' do
-      brands = ["Uggs", "Rainbow"]
-      brands.each do |brand|
-        Shoe.new(brand)
-      end
-
-      brands.each do |brand|
-        expect(Shoe::BRANDS).to include(brand)
-      end
-    end
-
-    it 'only keeps track of unique brands' do
-      Shoe::BRANDS.clear
-      brands = ["Uggs", "Rainbow", "Nike", "Nike"]
-      brands.each do |brand|
-        Shoe.new(brand)
-      end
-
-      expect(Shoe::BRANDS.size).to eq(3)
-    end
